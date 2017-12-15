b6c1eff64007c581ce44c6a237beb1695e1a7b17
Danny Kirschner <rkirschner1377@gmail.com>
2017-12-15 15:48:12 -0500

    Syncing up with ember auth template
    Adding various dependencies, setting to GA defaults

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('sign-up');
+  this.route('sign-in');
+  this.route('change-password');
+  this.route('users');
   this.route('scientists');
   this.route('about');
   this.route('list-rentals');
