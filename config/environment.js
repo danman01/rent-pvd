349145a0332f731dcc632db988759242de05da79
Danny Kirschner <rkirschner1377@gmail.com>
2018-01-01 19:19:28 -0500

    removing root-url for production

--- config/environment.js
+++ config/environment.js
@@ -50,7 +50,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/rent-pvd/';
+    ENV.rootURL = '/';
     // here you can enable a production-specific feature
     ENV.apiHost = 'https://rent-pvd.herokuapp.com/';
   }
