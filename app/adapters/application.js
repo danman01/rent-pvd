25b2d89237379dab55e74f2d027bf5f6ef47873d
Danny Kirschner <rkirschner1377@gmail.com>
2018-01-12 16:37:27 -0500

    Adding adapter that uses api namespace, commenting out our ember auth template adapter for now

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,20 +3,23 @@ import ActiveModelAdapter from 'active-model-adapter';
 
 import Ember from 'ember';
 
-export default ActiveModelAdapter.extend({
-  host: ENV.apiHost,
-
-  auth: Ember.inject.service(),
-
-  headers: Ember.computed('auth.credentials.token', {
-    get () {
-      let headers = {};
-      const token = this.get('auth.credentials.token');
-      if (token) {
-        headers.Authorization = `Token token=${token}`;
-      }
-
-      return headers;
-    },
-  }),
+//export default ActiveModelAdapter.extend({
+//  host: ENV.apiHost,
+//
+//  auth: Ember.inject.service(),
+//
+//  headers: Ember.computed('auth.credentials.token', {
+//    get () {
+//      let headers = {};
+//      const token = this.get('auth.credentials.token');
+//      if (token) {
+//        headers.Authorization = `Token token=${token}`;
+//      }
+//
+//      return headers;
+//    },
+//  }),
+import DS from 'ember-data';
+export default DS.JSONAPIAdapter.extend({
+  namespace: 'api'
 });
